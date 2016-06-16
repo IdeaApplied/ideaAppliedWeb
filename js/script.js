531490a4ad53dcb6a2b91a75661d2f15f1381fc6
Vijay Pratap Singh <vijay@ideaapplied.com>
2016-06-17 03:53:00 +0530

    Changes for tech icons, about us content

--- js/script.js
+++ js/script.js
@@ -173,10 +173,12 @@ $(document).ready(function() {
     
     $("#owl-demo").owlCarousel({
         autoPlay: 3000,
-        items: 4, //10 items above 1000px browser width
-        itemsDesktop: [1370, 3], //5 items between 1000px and 901px
-        itemsDesktopSmall: [900, 2], // betweem 900px and 601px
-        itemsTablet: [600, 1], //2 items between 600 and 0
+        items: 7, //10 items above 1000px browser width
+        itemsDesktop: [1370, 6], //5 items between 1000px and 901px
+        itemsDesktopSmall: [900, 4], //5 betweem 900px and 601px
+        itemsTablet: [600, 3], //3 items between 600 and 0,
+        itemsMobile: [479, 2],
+
     });
 });
 
